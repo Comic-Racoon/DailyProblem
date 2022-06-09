839b26f0f27a84eec9296f1a7d71886d523c505e
Rahul Mhatre <67099690+Comic-Racoon@users.noreply.github.com>
2022-06-09 17:36:45 +0530

    Add files via upload
    update recurssion

---
+++ ProblemADay/src/problems/recurssion/subset_subsequence/problem_4_subset_using_iteration.java
@@ -0,0 +1,33 @@
+package problems.recurssion.subset_subsequence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem_4_subset_using_iteration {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3};
+        List<List<Integer>> ans = subset(arr);
+        for (List<Integer> list : ans){
+            System.out.println(list);
+        }
+    }
+
+    static List<List<Integer>> subset(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+        for(int num : arr){
+            int n = outer.size();
+            for(int i =0; i< n; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+}
