2c18a289f3641464ceb46518a940803db58013b0
Rahul Mhatre <67099690+Comic-Racoon@users.noreply.github.com>
2022-06-09 17:38:57 +0530

    Add files via upload
    update concepts folder

---
+++ ProblemADay/src/problems/concepts/bfs_introduction/bfs.java
@@ -0,0 +1,24 @@
+package problems.concepts.bfs_introduction;
+
+import java.util.ArrayDeque;
+
+public class bfs {
+
+    // reove -> mark star -> work -> add star
+
+    static class Pair{
+        int num1;
+        int num2;
+
+        Pair(int i, int j ){
+            this.num1 = i;
+            this.num2 = j;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
--- ProblemADay/src/problems/concepts/dependency_injection/eg_2_emailApplication/EmailApplication.java
+++ ProblemADay/src/problems/concepts/dependency_injection/eg_2_emailApplication/EmailApplication.java
@@ -4,17 +4,16 @@ package problems.concepts.dependency_injection.eg_2_emailApplication;
 
         public static void main(String[] args) {
             EmailClient emailClient = new EmailClient(new BasicSpellChecker());
-            EmailClient emailClient2 = new EmailClient(new AdvanceSpellChecker());
+//            EmailClient emailClient2 = new EmailClient(new AdvanceSpellChecker());
 
             emailClient.sendEmail("Hey, " + "This is my 1st email message ");
             emailClient.sendEmail("Hey, " + "This is my 2st email message ");
         }
     }
 
-
-
     /*
 
     hear we are provideing dependencies in the email client class
         instead of creating it directly
+
     */
